1e292c5a1d77021957e701cd580e77e90520a05b
Lavkush Yadav <ly3731444@email.com>
2026-05-15 11:53:31 +0530

    add initial JavaScript files for tutorial content commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Introduce progaraming and javascript
+console.log('hello,Add three number 1,2,50');
+
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+// let c=addThreeNumbers(1, 2, 50);
+// console.log(c);
+
+ let result=addThreeNumbers(1, 2, 50);
+ console.log(result);
+
+ /* BOTH  ARE RUN  */
