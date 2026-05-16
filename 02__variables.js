097779e17d9776519c7888d49a35b5322e5b221c
Lavkush Yadav <ly3731444@email.com>
2026-05-16 12:25:21 +0530

    added javascript files

---
+++ 02__variables.js
@@ -0,0 +1,15 @@
+// using repl to test variables
+// variables are used to store data values
+// in JavaScript, we use the var, let, and const keywords to declare variables
+console.log('This repl contains code for ultimate JavaScript course day 2');
+
+var a=67;/// a contains 67
+// console.log(a);
+a="hello world";
+console.log(a);
+
+//let 8harry=7         //this is not valid variable name because it starts with a number
+let harry8=7; // this is valid variable name
+console.log(harry8);
+
+//let var=8     //not valid variable name because var is a reserved keyword in JavaScript
