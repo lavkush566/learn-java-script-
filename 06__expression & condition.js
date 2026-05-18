c3262947dc22213cc3576d3202f974466d18fd31
Lavkush Yadav <ly3731444@email.com>
2026-05-18 12:39:55 +0530

    Add JavaScript expressions, operators, and comments examples

---
+++ 06__expression & condition.js
@@ -0,0 +1,60 @@
+// 77;
+// "harry"
+// false      // this are run not show any error
+console.log("operator in js");
+// =====================================CHAPTER2 ARITHMETIC OPERATORS========================
+let a=10;
+let b=4;
+console.log("a+b",a+b); // it will give output as 14
+console.log("a-b",a-b); // it will give output as 6
+console.log("a*b",a*b); // it will give output as 40
+console.log("a/b",a/b); // it will give output as 2.5
+console.log("a%b",a%b); // it will give output as 2 because 10/4 is 2 and remainder is 2
+console.log("a**b",a**b); // it will give output as 10000 because 10 to the power 4 is 10000        
+console.log("++a",++a); // it will give output as 11 because it will first increment the value of a and then print it
+console.log("a++",a++); // it will give output as 11 because it will first print the value of a and then increment it
+console.log("--a",--a); // it will give output as 11 because it will first decrement the value of a and then print it
+console.log("a--",a--); // it will give output as 11 because it will first print the value of a and then decrement it
+
+// =====================================CHAPTER2 ASSIGNMENT OPERATORS========================
+let assignment=1;
+assignment+=5; //same as assignment = assignment + 5
+console.log(" a is now =",a);
+assignment-=5; //same as assignment = assignment - 5
+console.log(" a is now =",a);
+assignment*=5; //same as assignment = assignment * 5
+console.log(" a is now =",a);
+assignment/=5; //same as assignment = assignment / 5
+console.log(" a is now =",a);
+assignment%=5;  //same as assignment = assignment % 5
+console.log(" a is now =",a);
+
+
+// =====================================CHAPTER2 COMPARISON OPERATORS========================
+let comp1=6;
+let comp2=7;
+console.log("comp1 == comp2 is",comp1 == comp2);
+console.log("comp1 != comp2 is", comp1 != comp2);
+console.log("comp1 === comp2 is", comp1 === comp2);
+console.log("comp1 !== comp2 is", comp1 !== comp2);
+console.log("comp1 > comp2 is",comp1 >comp2);
+console.log("comp1 < comp2 is",comp1 <comp2);
+console.log("comp1 <= comp2 is",comp1<= comp2);
+console.log("comp1 >= comp2",comp1 >= comp2);
+
+ // =====================================CHAPTER2 LOGICAL OPERATORS========================
+let x =5;
+let y = 6;
+console.log(x<y && x==5); //Explanation of && operator
+console.log(x>y || x==5);//Explanation of || operator
+console.log(!true)//Explanation of ! operator
+
+
+ // =====================================CHAPTER2 COMMENTS ========================
+//this is single line comment 
+
+/* This is multipal line comment
+FROM MORE 
+CODE WITH HARRY 
+AND PRACTICE DAILY 
+*/
