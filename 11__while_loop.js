8f3925b9eecc55ad2dd59b77a0372f358fecce47
Lavkush Yadav <ly3731444@email.com>
2026-05-27 14:58:43 +0530

    This is while and do while loop

---
+++ 11__while_loop.js
@@ -0,0 +1,19 @@
+// while loop
+let n= ("Enter the value of n")
+n= Number.parseInt(n)
+
+let i=0;
+while(i<0){
+    console.log(i);
+    i++;
+}
+
+//do while loop
+let n=prompt("Enter the value of n");
+n=Number.parseInt(n);
+
+let i=10;
+do{
+    console.log(i);
+    i++;
+}while(i<n);
