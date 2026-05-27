2f65bb7a1e120c0b0519a045a9a76f76703b4a31
Lavkush Yadav <ly3731444@email.com>
2026-05-27 16:02:13 +0530

    This is a function

---
+++ 12_function.js
@@ -0,0 +1,22 @@
+//function
+//new  mwthode
+const hello=()=>{
+    console.log("Hey how are you. I am toh fine yaar");
+}
+
+function oneplusAvg(x,y){
+    return 1+(x+y)/2;
+}
+
+const sum =(p,q)=>{
+    return p+q;
+}
+
+let a =1;
+let b=2;
+let c=3;
+hello();
+console.log("one plus Average of a and b  is",oneplusAvg(a,b));
+console.log("one plus Average of b and c  is",oneplusAvg(b,c));
+console.log("one plus Average of a and c  is",oneplusAvg(a,c));
+console.log("sum of a and b is",sum(9,7));
