f8648c2df91b7fcca610db589cfd6ec0f1103939
Lavkush Yadav <ly3731444@email.com>
2026-06-01 11:40:34 +0530

    this is string

---
+++ 14 string.js
@@ -0,0 +1,19 @@
+ // string\
+ let name = "harry";
+//  console.log(name.length);'
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+let friend = 'rohan';
+console.log(friend);
+let myfrind = 'harry"; // this is  not working because we are using single and double quotes
+console.log(myfrind);
+
+ // Template literals
+let boy1 ="pramod"
+let boy2 ="Nikhil"
+// nikhil is a friend of promod
+let sentence =`${boy2} is a friend of ${boy1}`
+console.log(sentence);
+let fruit= 'bana\'na'
+consolel.log(fruit);
