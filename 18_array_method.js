a6b35c2b3b21ed682de25c18d00bb25af40d372c
Lavkush Yadav <ly3731444@email.com>
2026-06-03 12:26:36 +0530

    This is  a array method

---
+++ 18_array_method.js
@@ -0,0 +1,14 @@
+// Array method\
+let num=[1,2,3,34,4]
+let b=num.toString() // b is now a string
+console.log(b,typeof b);
+let c=num.join("-") // we use and also
+console.log(c,typeof c);
+let r=num.pop() // pop return the popped element
+console.log(r,typeof r);
+let r1=num.push(56) // push return the new length of the array
+console.log(r1,num);
+let r2=num.shift() // shift return the shifted element
+console.log(r2,num);
+let r3=num.unshift(67) 
+console.log(r3,num);  
