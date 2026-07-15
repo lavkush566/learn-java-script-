cb08b7e4db776839c6a5cff9bcfa2b85bd6f59ba
Lavkush Yadav <ly3731444@email.com>
2026-07-15 11:17:30 +0530

    This  is classlist and classname

---
+++ html css and java script/34_ClassName_classlist/script.js
@@ -0,0 +1,7 @@
+first.className ="text-dark yellow"
+first.classList.add("red");
+first.classList.remove("yellow");
+first.classList.toggle("blue");
+first.classList.toggle("red");
+first.classList.replace("text-dark", "text-light");
+first.classList.contains("text-light");
