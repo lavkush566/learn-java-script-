0b08c2fdc0bb1bf8ff52a7102e833e1f56274642
Lavkush Yadav <ly3731444@email.com>
2026-07-15 10:21:42 +0530

    This is insert.adjacentHTML

---
+++ html css and java script/33_insert_adjacentHTML/script.js
@@ -0,0 +1,5 @@
+first.insertAdjacentHTML('beforebegin', '<div class="test">BeforeBegin</div>');
+first.insertAdjacentHTML('afterbegin', '<div class="test">After Begin</div>');
+first.insertAdjacentHTML('beforeend', '<div class="test">Before End</div>');
+first.insertAdjacentHTML('afterend', '<div class="test">After End</div>');
+// first.remove;
