4e133a8e6197a20649a38b9ef33ebc8a0285c6f6
Lavkush Yadav <ly3731444@email.com>
2026-07-18 12:14:31 +0530

    this is for addEventListener and removeEventListener

---
+++ html css and java script/37_addEventListener_removeEventlistener/script.js
@@ -0,0 +1,18 @@
+let x = function(e){
+    alert("10")
+}
+
+let y = function (e){
+    alert("Hello world9")
+}
+
+btn.addEventListener('click', x)
+
+// btn.addEventListener('click',y)
+
+// let a = prompt("What is your favorite number?");
+
+// if(a == 7){
+//     btn.removeEventListener('click', x) 
+
+// }
