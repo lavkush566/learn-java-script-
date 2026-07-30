66daba0f43ac3b0cd8014ca9b02311dab8477a62
Lavkush Yadav <ly3731444@email.com>
2026-07-30 13:55:23 +0530

    Error handling

---
+++ html css and java script/48__Error_handling/script.js
@@ -0,0 +1,23 @@
+setTimeout(()=>{
+    console.log("hacking wifi.....please wait");
+},1000);
+
+try{
+    console.log("rahul")
+}
+catch(error){
+    console.log(error);
+}
+
+setTimeout(()=>{
+    console.log("Featching username and password.....please wait");
+    
+},2000);
+
+setTimeout(()=>{
+    console.log("Hacking rahul's fackbook id.....please wait");
+},3000);
+
+setTimeout(()=>{
+    console.log("Username and password of Rahul(+918576945720) fetched.....please wait");
+},4000);
