b2d30e5c76201a13be5f7454a39783e03aa33c3e
Lavkush Yadav <ly3731444@email.com>
2026-08-18 12:48:36 +0530

    session storage

---
+++ html css and java script/55_sessionStorage/script.js
@@ -0,0 +1,11 @@
+/*sessionStorage.setItem("username", "JohnDoe");
+sessionStorage.clear();
+sessionStorage.removeItem("username");
+sessionStorage.getItem("username");
+sessionStorage.key(0);*/
+
+
+window.onstorage   =(e)=>{
+    alert("change in storage");
+}
+ 
